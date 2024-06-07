3b9e6ff01304e15d6a705374e6b78be2dd1f076e
chakripavankumar <siriyalapavankumar@gmail.com>
2024-06-07 18:56:44 +0530

    upload service

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,40 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const simple_git_1 = __importDefault(require("simple-git"));
+const utils_1 = require("./utils");
 const app = (0, express_1.default)();
-app.use(cors_1.default);
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.post("/deploy", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const repoUrl = req.body.repoUrl;
+    const id = (0, utils_1.generate)();
+    try {
+        yield (0, simple_git_1.default)().clone(repoUrl, `files/${id}`);
+        res.json({ id: id });
+    }
+    catch (error) {
+        console.error("Error during cloning:", error);
+        res.status(500).json({ error: "Failed to clone repository" });
+    }
+}));
+app.get("/test", (req, res) => {
+    console.log("hey there");
+    res.send("Hello, world!");
+});
 app.listen(3000, () => {
-    console.log("app is successfully listen in 3k");
+    console.log("Server is running on port 3000");
 });
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,25 @@
-import express from "express"
+import express from "express";
 import cors from "cors";
+import simpleGit from "simple-git";
+import { generate } from "./utils";
 
-
-
-const app= express();
-app.use(cors);
+const app = express();
+app.use(cors());
 app.use(express.json());
 
-
-app.post("/deploy " , async (req,res)=>{
-    const repourl= req.body.repourl;
-    console.log(repourl);
-
-    res.json({})
-    
-})
-
-
-
-
-
+app.post("/deploy", async (req, res) => {
+    const repoUrl = req.body.repoUrl;
+    const id = generate();
+    try {
+        await simpleGit().clone(repoUrl, `files/${id}`);
+        res.json({ id: id });
+    } catch (error) {
+        console.error("Error during cloning:", error);
+        res.status(500).json({ error: "Failed to clone repository" });
+    }
+});
 
 
-
-
-
-app.listen(3000, ()=>{
-    console.log("app is successfully listen in 3k");
-    
-})
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
